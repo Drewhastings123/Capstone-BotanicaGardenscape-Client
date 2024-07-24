ab04d685d69ee18c1fa33ba6d8aaa11fd61af9b8
Drew Hastings <drewhastings@Drews-MacBook-Pro.local>
2024-07-24 13:58:23 -0400

    testPlants

--- src/components/Garden.jsx
+++ src/components/Garden.jsx
@@ -67,11 +67,10 @@ export default function Garden({ shape, setShape }) {
           <select
             className="custom-select form-control input-sm p-1"
             onChange={updateShape}
+            defaultValue="sq"
           >
             <option>Shape</option>
-            <option value="sq" selected>
-              Square
-            </option>
+            <option value="sq">Square</option>
             <option value="rec">Rectangle</option>
             <option value="cir">Circle</option>
           </select>
@@ -81,12 +80,11 @@ export default function Garden({ shape, setShape }) {
           <select
             className="custom-select form-control input-sm p-1"
             onChange={updateShape}
+            defaultValue="sq"
           >
             <option>Shape</option>
             <option value="sq">Square</option>
-            <option value="rec" selected>
-              Rectangle
-            </option>
+            <option value="rec">Rectangle</option>
             <option value="cir">Circle</option>
           </select>
         );
@@ -95,13 +93,12 @@ export default function Garden({ shape, setShape }) {
           <select
             className="custom-select form-control input-sm p-1"
             onChange={updateShape}
+            defaultValue="sq"
           >
             <option>Shape</option>
             <option value="sq">Square</option>
             <option value="rec">Rectangle</option>
-            <option value="cir" selected>
-              Circle
-            </option>
+            <option value="cir">Circle</option>
           </select>
         );
 
@@ -110,11 +107,10 @@ export default function Garden({ shape, setShape }) {
           <select
             className="custom-select form-control input-sm p-1"
             onChange={updateShape}
+            defaultValue="sq"
           >
             <option>Shape</option>
-            <option value="sq" selected>
-              Square
-            </option>
+            <option value="sq">Square</option>
             <option value="rec">Rectangle</option>
             <option value="cir">Circle</option>
           </select>
@@ -187,10 +183,8 @@ export default function Garden({ shape, setShape }) {
 
   return (
     <>
-
       {/* <Nav_Bar /> */}
       <div className="container-fluid w85 ">
-
         {/* < Loading_Bar /> */}
 
         <div className="row w100 ">
